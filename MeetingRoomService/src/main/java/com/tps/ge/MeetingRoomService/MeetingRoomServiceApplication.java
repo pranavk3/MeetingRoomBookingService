f9905aef691a985f095019a2055c638cfb72f0c5
pranavkatkar <pranav.katkar@niit.com>
2018-09-02 13:40:51 +0530

    created project

---
+++ MeetingRoomService/src/main/java/com/tps/ge/MeetingRoomService/MeetingRoomServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tps.ge.MeetingRoomService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomServiceApplication.class, args);
+	}
+}
