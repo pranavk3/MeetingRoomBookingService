c8c725d7c5d69dbbcd592bf1b6438294e67d82fc
pranavkatkar <pranav.katkar@niit.com>
2018-09-02 14:52:24 +0530

    revised Project

---
+++ meetingRoomService/src/main/java/com/ge/tps/meetingRoomService/MeetingRoomServiceApplication.java
@@ -0,0 +1,21 @@
+package com.ge.tps.meetingRoomService;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MeetingRoomServiceApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(MeetingRoomServiceApplication.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
